d8d1ed9809fa6becd4622a4ced49768047fea145
Nathan Muncy <nmuncy2@unl.edu>
2025-08-07 17:06:16 -0500

    [DEV] testing publish

---
+++ docs/code/the_terminal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Command: options and parameters
+echo Hello World # Print
+echo -e "Hello\nWorld" # Print with new line
+
+# Foundational commands
+cd # Go home
+ls # List contents of home
+ls -la # List all contents of home and permissions
+ls / # List contents of root
+pwd # Print working directory
+
+# Making directories
+cd ~/Desktop # Go to desktop
+mkdir -p foo/bar # Make directory structure
+tree foo # Check directory structure
