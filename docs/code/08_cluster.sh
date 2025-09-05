46f0a765e65802a7a7c391832b0463f9e81280c0
Nathan Muncy <nmuncy2@unl.edu>
2025-09-05 15:32:02 -0500

    [DEV] finished 08

---
+++ docs/code/08_cluster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Syntax for SSH connection
+ssh user@crcfe01.crc.nd.edu
+
+# QSUB syntax for checking nodes
+#$ -pe smb 2
+#$ -q long
+shopt -q login_shell && echo login || echo non-login
+[[ $- == *i* ]] && echo interactive || echo non-interactive
+
+# Submit qsub script 'test_nodes.sh' as job named 'node_check'
+qsub -N node_check test_nodes.sh
+
+# Check CRC home usage
+quota
+
+# Check CRC scratch usage
+pan_df -H /scratch365/$(whoami)
+
+# Scp
+scp file.txt user@crcfe01.crc.nd.edu:~
+scp -r user@crcfe01.crc.nd.edu:~/project ~/Desktop
+
+# Rsync
+rsync -rauv ~/project user@crcfe01.crc.nd.edu:~
